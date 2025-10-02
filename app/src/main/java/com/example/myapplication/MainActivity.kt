03aede66659955d481bd4317fce23d253e1fc134
Nikusha Papalashvili <papalashvilinika6@gmail.com>
2025-10-03 02:19:15 +0400

    Homework 5

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,13 +1,14 @@
 package com.example.myapplication
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.myapplication.databinding.ActivityMainBinding
+import kotlin.collections.component1
+import kotlin.collections.component2
+import kotlin.collections.iterator
 
 
 class MainActivity : AppCompatActivity() {
@@ -21,89 +22,53 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.mainId)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        val fields = listOf(
-            binding.emailId,
-            binding.userNameId,
-            binding.firstNameId,
-            binding.lastNameId,
-            binding.ageId
-        )
+        val usersInfo = mutableMapOf<String, String>()
 
-        clearButton(fields)
-        saveButton(fields)
-
+        addButton(usersInfo)
+        getButton(usersInfo)
     }
 
-    private fun clearButton(list: List<EditText?>) {
-        binding.clearBtnId?.setOnLongClickListener {
-            for(i in list) i?.text?.clear()
-            true
-        }
-    }
 
-    private fun errorMessage(list: List<EditText?>) : Boolean {
-        val isEmpty = list.any { it?.text?.isEmpty() == true }
-        binding.resultTextId?.text = ""
-        var isError = false
-
-        if (isEmpty) {
-            binding.resultTextId?.text = getString(R.string.emptyMsg)
-            isError = true
-        }
-
-        if (binding.emailId?.text?.contains('@') == false) {
-            binding.emailId?.error = "Please enter real mail"
-            isError = true
-        }
-
-        if ((binding.userNameId?.text?.length ?: 0) < 10) {
-            binding.userNameId?.error = "Please enter more than 10 characters"
-            isError = true
-        }
-
-        if (binding.firstNameId?.text?.isEmpty() == true) {
-            binding.firstNameId?.error = "Please enter first name"
-            isError = true
-        }
-
-        if (binding.lastNameId?.text?.isEmpty() == true) {
-            binding.lastNameId?.error = "Please enter last name"
-            isError = true
-        }
-
-        if (binding.ageId?.text?.isEmpty() == true) {
-            binding.ageId?.error = "Please enter Age"
-            isError = true
-        }
-        return isError
-    }
-
-    private fun saveButton(list: List<EditText?>) {
-        binding.saveBtnId?.setOnClickListener {
-
-            if(!errorMessage(list)) {
-                val username = binding.userNameId?.text.toString()
-                val email = binding.emailId?.text.toString()
-                val name = binding.firstNameId?.text.toString() + " " + binding.lastNameId?.text.toString()
-                val age : Int? = binding.ageId?.text.toString().toIntOrNull()
-
-                val intent = Intent(this, InfoPageActivity::class.java).apply {
-                    putExtra("username",username)
-                    putExtra("email",email)
-                    putExtra("name",name)
-                    putExtra("age",age)
+    fun getButton(usersInfo : MutableMap <String, String>) {
+        binding.getUserBtnId.setOnClickListener {
+            for ((key, value) in usersInfo) {
+                if(key == binding.inputEmailId.text.toString()) {
+                    binding.resultTextId.text = "Email: $key"
+                    binding.resultText2Id.text = "Username $value"
+                } else {
+                    binding.resultTextId.text = "Account Cannot Found"
+                    binding.resultText2Id.text = ""
                 }
-                startActivity(intent)
             }
 
         }
+    }
 
+    fun addButton(usersInfo: MutableMap<String, String>) {
+        binding.addUserBtnId.setOnClickListener {
+            val email = binding.inputEmailId.text.toString()
+            val name = binding.inputNameId.text.toString()
+
+            if('@' in email && name.isNotEmpty()){
+                if(email in usersInfo.keys) {
+                    binding.inputEmailId.error = "This Email Already Registered"
+                }else {
+                    usersInfo[email] = name
+                    binding.usersNumTextId.text = binding.usersNumTextId.text?.dropLast(1)
+                    binding.usersNumTextId.append(usersInfo.size.toString())
+                }
+            }else {
+                if(!email.contains('@')) binding.inputEmailId.error = "Enter Correct Email"
+                if(name.isEmpty()) binding.inputNameId.error = "Enter Name To Add"
+            }
+
+        }
     }
 
 }
