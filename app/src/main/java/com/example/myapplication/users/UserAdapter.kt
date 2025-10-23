88835051864a7b5642ea740c2052f6d7ed565d0a
Nikusha Papalashvili <papalashvilinika6@gmail.com>
2025-10-24 01:03:21 +0400

    revert commit.

---
+++ app/src/main/java/com/example/myapplication/users/UserAdapter.kt
@@ -0,0 +1,45 @@
+package com.example.myapplication.users
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.myapplication.User
+import com.example.myapplication.databinding.UserItemBinding
+
+class UsersAdapter(
+    private val users: List<User>,
+    private val onItemClick: (User) -> Unit
+) :
+    RecyclerView.Adapter<UsersAdapter.UserViewHolder>() {
+
+    inner class UserViewHolder(private val binding: UserItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(user: User) {
+            binding.tvFullName.text = "${user.firstName} ${user.lastName}"
+            binding.tvAge.text = "Age: ${user.age}"
+            binding.tvEmail.text = user.email
+
+            binding.root.setOnLongClickListener {
+                onItemClick(user)
+                true
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
+        val binding = UserItemBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+        return UserViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
+        holder.bind(users[position])
+    }
+
+    override fun getItemCount(): Int = users.size
+}
+
+
