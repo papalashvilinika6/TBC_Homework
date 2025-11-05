742e76e808add385e2c5774dc91c9d76e81d42b7
Nikusha Papalashvili <papalashvilinika6@gmail.com>
2025-11-05 04:18:44 +0400

    Homework 16 FINAL

--- app/src/main/java/com/example/myapplication/chat/ChatFragment.kt
+++ app/src/main/java/com/example/myapplication/chat/ChatFragment.kt
@@ -1,7 +1,9 @@
 package com.example.myapplication.chat
 
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.myapplication.chat_message.ChatAdapter
 import com.example.myapplication.common.BaseFragment
@@ -14,19 +16,18 @@ class ChatFragment : BaseFragment<FragmentChatBinding>(FragmentChatBinding::infl
     private lateinit var adapter: ChatAdapter
 
     override fun listeners() {
+        btnSend()
     }
 
     override fun bind() {
-        adapter = ChatAdapter()
-        binding.rvChatId.adapter = adapter
-        binding.rvChatId.layoutManager = LinearLayoutManager(requireContext()).apply { reverseLayout = true }
+        bindRecyclerView()
+    }
 
-        lifecycleScope.launch {
-            viewModel.messages.collect { list ->
-                adapter.submitList(list)
-            }
-        }
+    override fun observers() {
+        observeMessages()
+    }
 
+    private fun btnSend() {
         binding.btnSendId.setOnClickListener {
             val text = binding.etMessageInputId.text.toString()
             viewModel.sendMessage(text)
@@ -34,7 +35,21 @@ class ChatFragment : BaseFragment<FragmentChatBinding>(FragmentChatBinding::infl
         }
     }
 
-    override fun observers() {
+    private fun observeMessages() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.messages.collect { list ->
+                    adapter.submitList(list)
+                }
+            }
+        }
+    }
+
+    private fun bindRecyclerView() {
+        adapter = ChatAdapter()
+        binding.rvChatId.adapter = adapter
+        binding.rvChatId.layoutManager =
+            LinearLayoutManager(requireContext()).apply { reverseLayout = true }
     }
 
 
