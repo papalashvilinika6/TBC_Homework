53967626faa4948fad6e3a521c7fa1bd997301f8
Nikusha Papalashvili <papalashvilinika6@gmail.com>
2025-12-14 17:27:50 +0400

    Api change

--- app/src/main/java/com/example/myapplication/data/remote/DriversApi.kt
+++ app/src/main/java/com/example/myapplication/data/remote/DriversApi.kt
@@ -4,6 +4,6 @@ import com.example.myapplication.data.dto.DriversResponseDto
 import retrofit2.http.GET
 
 interface DriversApi {
-    @GET("1168ef2b-4423-4e5f-9043-a3f9e93d4807")
+    @GET("4932c17f-f218-415a-9875-4831f836723b")
     suspend fun getDrivers(): DriversResponseDto
 }
