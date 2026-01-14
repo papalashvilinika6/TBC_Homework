0e7cd943e82c61e79bf89790c9a73e069141fd27
Papala15 <papalashvilinika6@gmail.com>
2026-01-14 07:28:10 +0400

    Homework-32

--- app/src/main/java/com/example/challenge/presentation/screen/splash/SplashUiEvent.kt
+++
@@ -1,6 +0,0 @@
-package com.example.challenge.presentation.screen.splash
-
-sealed class SplashUiEvent {
-    data object NavigateToLogIn : SplashUiEvent()
-    data object NavigateToConnections : SplashUiEvent()
-}
